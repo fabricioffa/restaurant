815da50cdab549e4db87828831bf64461f9e40b0
fabricio <fabricioffa08@gmail.com>
2024-12-03 06:01:21 -0300

    add better-sqlite3 types

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,8 +1,6 @@
 import Database from 'better-sqlite3';
 import { singleton } from "~/singleton.server";
 
-
-// Hard-code a unique key, so we can look up the client when this module gets re-imported
 const db = singleton("db", () => new Database('vilicus.db', { verbose: console.log }));
 db.pragma('journal_mode = WAL');
 
